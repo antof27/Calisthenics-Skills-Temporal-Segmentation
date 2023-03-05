38de8f207f7b95ed858dff82ed87ff1cf06a1325
antof27 <86187212+antof27@users.noreply.github.com>
2023-03-05 15:50:30 +0100

    Update labels_elaboration.py

--- post_nn_algorithm_NC/labels_elaboration.py
+++ post_nn_algorithm_NC/labels_elaboration.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from scipy.stats import mode
+import matplotlib.pyplot as plt
 import numpy as np
 
 
@@ -70,6 +71,12 @@ for i in range(0, len(modes)):
 print(modes)
 #print(final_array)
 
+'''
+total_frame = (output[-1][2])+1
+total_length = total_frame*(1/24)
+print("total length in seconds: ", total_length)
+'''
+
 output = []
 output_l = []
 i = 0
@@ -86,14 +93,54 @@ while i < len(modes)-1:
 
     
     if breakp == False:
-        end = (modes[i][1])-1
+        end = (modes[i][1]-1)
     
     output.append([skill, start, end])
-    milliseconds = (end-start)*(1/24)
+    milliseconds = ((end+1)-start)*(1/24)
     output_l.append([skill, milliseconds])
 
 print("\nLista finale: \n")
 print(output)
+
 print("\nLista finale in millisecondi: \n")
 print(output_l)
+
+fig, (ax1, ax2) = plt.subplots(2, 1)
+
+x = []
+y = []
+for i in range(0, len(output)):
+    x.append(output[i][1])
+    x.append(output[i][2])
+    y.append(output[i][0])
+    y.append(output[i][0])
+
+#ax1.subplot(2, 1, 1)
+ax1.plot(x, y)
+ax1.set_title("Timeline in frames")
+ax1.set_xlabel("Frames")
+ax1.set_ylabel("Skills")
+
+x1 = []
+y1= []
+k = 0
+for i in range(0, len(output_l)):
+    x1.append(k)
+    x1.append(k+output_l[i][1])
+    y1.append(output_l[i][0])
+    y1.append(output_l[i][0])
+    k+=output_l[i][1]
+
+print("x1", x1)
+print("y1", y1)
+
+
+#ax2.subplot(2, 1, 2)   
+ax2.plot(x1, y1, color='red')
+ax2.set_title("Timeline in seconds")
+ax2.set_xlabel("Seconds")
+ax2.set_ylabel("Skills")
+
+fig.tight_layout()
+plt.show()
 
