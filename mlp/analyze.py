bbda52b5f99a092d36cff55118601893da8c8a58
antof27 <86187212+antof27@users.noreply.github.com>
2023-05-11 11:54:12 +0200

    Update analyze.py

--- mlp/analyze.py
+++ mlp/analyze.py
@@ -10,87 +10,120 @@ oafl 6
 oahs 7
 pl 8
 '''
-
-
-def edge_analyzer(l1):
-#save the index of the first element not equal to 5
-
+def none_sequence(l1, k):
     first_index = 0
     last_index = 0
-
-    if l1[0] == 5:
-        for i in range(len(l1)):
-            if l1[i] != 5:
-                first_index = i
-                break
-        
-
-    if l1[-1] == 5:
-        for i in range(len(l1)-1, -1, -1):
-            if l1[i] != 5:
-                last_index = i
-                break
- 
-    #initialize l2 with the same length as l1
-    if first_index == 0 and last_index == 0:
-        return
-
-    if first_index == 0 and last_index != 0:
-        first_index = last_index
-
-    if first_index != 0 and last_index == 0:
-        last_index = first_index
     
-    print("First index", first_index)
-    print("Last index", last_index)
-    l2 = index_list_filler(l1, first_index, last_index)
+    if k-1 >= 0:
+        first_index = k-1
+    else:
+        first_index = 0
+
+    #read all the sequence of 5
+    while k < len(l1) and l1[k] == 5:
+        k+=1
+    #save the index of the last element not equal to 5
+    last_index = k
+
+    return first_index, last_index
+
+def edge_analyzer(l1):
+    indeces = []
+    k = 0
+    while k < len(l1):
+        
+        if l1[k] != 5:
+            k+=1
+            continue
+
+        f,l= none_sequence(l1, k)
+        if f != 0 :
+            indeces.append(f)
+        if l != len(l1):
+            indeces.append(l)
+        
+        
+        
+        k = l+1
+        
+    if len(indeces) == 0:
+        return []
+    
+    l2 = index_list_filler(l1, indeces)
     return l2
 
 
         
-def index_list_filler(l1, first_index, last_index):
+def index_list_filler(l1, indeces):
     l2 = np.ones(len(l1))
-    l2[first_index] = 0
-    l2[last_index] = 0
-
 
     for i in range(len(l2)):
-        for j in range(i, first_index):
-            l2[j] = first_index - j
+        if i in indeces:
+            l2[i] = 0
 
-        k = first_index+1
-        y = last_index-1
-        fill = 1
+    for i in range(len(l2)):
+
+        for j in range(i, indeces[0]):
+            l2[j] = indeces[0] - j
         
-        while k <= y:
-            l2[k] = fill
-            l2[y] = fill
-            fill+=1
-            k+=1
-            y-=1
+        for y in range(0, len(indeces)):
+            s = indeces[y]+1
+            #add a control to avoid out of range
+            if y+1 >= len(indeces):
+                break
 
-        for h in range(last_index+1, len(l2)):
-            l2[h] = h - last_index
+            e = indeces[y+1]-1
+            
+            fill = 1
+            while s <= e:
+                
+                if e >= len(l2):
+                    break
+                l2[s] = fill
+                l2[e] = fill
+                fill+=1
+                s+=1
+                e-=1
+        
+        
+        
+        for h in range(indeces[-1]+1, len(l2)):
+            l2[h] = h - indeces[-1]
     
     return l2
   
 
 
-def range_accuracy_checker(raw_predicted, gt_test, lista_finale, interval):
+def range_accuracy_checker(raw_predicted, gt_test, lista_finale, limit):
+    
     total = 0
     true = 0
     max_interval = 0
+    truetotal_list = [[0, 0] for _ in range(limit)]
+
+
     for i in range(len(lista_finale)):
         if lista_finale[i] == 0.0:
             
-            counter = 1
-            while counter <= interval:
-                
-                #print("i-counter", gt_test[i-counter], "[",i-counter,"]", "i+counter", gt_test[i+counter], "[",i+counter,"]")
-                #check if the index is in the range
+            counter = 0
+            
+            while counter < limit:
+                total = 0
+                true = 0
+                if counter == 0:
+                    #print("Compare : ", raw_predicted[i], " with ", gt_test[i])
+                    if raw_predicted[i] == gt_test[i]:
+                        true+=1
+                    total+=1
+
+                    truetotal_list[counter][0] += true
+                    truetotal_list[counter][1] += total 
+                    counter+=1
+                    continue
+
                 if i-counter >= 0 and i+counter < len(lista_finale):
-                    if lista_finale[i-counter] == lista_finale[i+counter]:
-                        #print("lista[i-counter]", lista_finale[i-counter], "lista[i+counter]", lista_finale[i+counter]," minore di ", interval)
+
+                    if lista_finale[i-counter] == counter and lista_finale[i+counter] == counter:
                         
                         if raw_predicted[i-counter] == gt_test[i-counter]:
                             true+=1
@@ -99,46 +132,49 @@ def range_accuracy_checker(raw_predicted, gt_test, lista_finale, interval):
                             true+=1
                         
                         total +=2
+
+
+                        truetotal_list[counter][0] += true
+                        truetotal_list[counter][1] += total 
+
                     else:
-                        max_interval = counter
-                        
-                        
+                        max_interval = counter-1
                         break
                     counter+=1
+                else:
+                    max_interval = counter-1
+                    break
 
-    return true/total, max_interval
+    return truetotal_list
                     
 
             
-#call the function with the range
-def analyzer(raw_predicted, gt_test, limit = 10):
-    
+#call the function with the wanted range
+def analyzer(raw_predicted, gt_test, limit):
     accuracy_list = np.zeros(limit)
-    #build the lista_finale
-    lista_finale = edge_analyzer(gt_test)
-    print("Lista finale: ", lista_finale)
-    print("Lista gt: ", gt_test)
-    print("Lista raw: ", raw_predicted)
-
-    for j in range(1, limit):
-        accuracy, max_interval = range_accuracy_checker(raw_predicted, gt_test, lista_finale, j)
-        if j >= max_interval and max_interval != 0:
-            break
-
-        accuracy_list[j-1]=accuracy
-
-    print("Accuracy list: ", accuracy_list)
-
-    return accuracy_list
+    lista_finale = edge_analyzer(gt_test)    
+    if len(lista_finale) == 0:
+        return accuracy_list
     
+    truetotal_list = range_accuracy_checker(raw_predicted, gt_test, lista_finale, limit)
 
-
-
+    return truetotal_list
+ 
 '''
-gt_test = [5,5,5,5,5,6,6,6,6,6,6,6,6,6,5,5,5,5,5,5,5,5,5] 
-raw_predicted = [5,5,5,5,5,6,7,6,6,7,6,6,7,6,5,5,5,5,5,5,5,5,5]
-accuracy_list = analyzer(raw_predicted, gt_test)
+gt_ =  [5,5,5,5,5,5,5,6,6,6,6,6,6,6,5,5,5,5,5,5,3,3,3,3,3,3,3,3,5,5,5,5,5]
+raw_ = [5,5,5,5,5,5,5,7,6,8,6,5,6,1,5,5,5,5,5,5,2,4,3,9,3,2,3,3,5,5,5,5,5]
+
+
+
+print(raw_)
+print(gt_)
+
+acc1 = analyzer(raw_, gt_, 6)
+print("Accuracy: ", acc1)
 '''
+
+
+
 
 
 
