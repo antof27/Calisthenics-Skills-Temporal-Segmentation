b18e3b739d0f7deff7a4fa3fc5d59383d4f26823
antof27 <86187212+antof27@users.noreply.github.com>
2023-06-25 16:56:54 +0200

    Add files via upload

---
+++ demo/demo_matrix.py
@@ -0,0 +1,103 @@
+from PIL import Image, ImageDraw, ImageFont
+import os
+
+def decoding(idx):
+    skill_colors = [(220, 20, 60), (21, 176, 26), (3, 67, 223), (255, 170, 0), (19, 185, 191), (60, 60, 60), (169,86,30), (249, 115, 6), (218, 112, 214)]
+    if idx == 0:
+        clr = skill_colors[0]
+    elif idx == 1:
+        clr = skill_colors[1]
+    elif idx == 2:
+        clr = skill_colors[2]
+    elif idx == 3:
+        clr = skill_colors[3]
+    elif idx == 4:
+        clr = skill_colors[4]
+    elif idx == 5:
+        clr = skill_colors[5]
+    elif idx == 6:
+        clr = skill_colors[6]
+    elif idx == 7:
+        clr = skill_colors[7]
+    elif idx == 8:
+        clr = skill_colors[8]
+
+    return clr
+
+def draw_horizontal_bars(matrix, est_list, gt_list):
+    # Dimensions of the image
+    image_width = 450
+    image_height = 540
+    environment = os.getcwd()
+    font_size = 25
+    # Number of bars and height of each bar
+    font_path = environment + "/arial.ttf"
+    font = ImageFont.truetype(font_path, font_size)
+    raw_text = "RAW probabilities"
+    est_text = "VSR"
+    gt_text = "GT"
+    image = Image.new("RGB", (image_width, image_height), "white")
+    draw = ImageDraw.Draw(image)
+
+    draw.text((10, 5), raw_text, fill=(0,0,0), font=font)
+    draw.text((290, 5), est_text, fill=(0,0,0), font=font)
+    draw.text((390, 5), gt_text, fill=(0,0,0), font=font)
+    
+    num_bars = len(matrix[0])
+    
+    bar_height = (image_height-50) // num_bars
+
+    # Create an image with white background
+    
+
+    # Draw bars for each row
+    for row_idx, row in enumerate(matrix):
+        # Draw bars for each column
+        #draw a circle for the estimated value
+        color_e = decoding(est_list)
+        shape = [(300, est_list*bar_height+62), (324, est_list*bar_height+86)]
+        draw.ellipse(shape, color_e)
+
+        color_gt = decoding(gt_list)
+        shape2 = [(394, gt_list*bar_height+62), (418, gt_list*bar_height+86)]
+        draw.ellipse(shape2, color_gt)
+
+
+        for col_idx, value in enumerate(row):
+            # Calculate the width of the bar
+            bar_width = int(value * (image_width-200))
+
+            # Calculate the position of the bar
+            x1 = 5
+            y1 = col_idx * bar_height +50
+            x2 = bar_width+5
+            y2 = (col_idx + 1) * bar_height+40
+
+            # Draw the bar
+            color = decoding(col_idx)
+            draw.rectangle([(x1, y1), (x2, y2)], fill=color)
+
+    return image
+'''
+# Example usage
+matrix = [
+    [0.1, 0.05, 0.5, 0.02, 0.1, 1],
+    [4.8928469e-1, 6.1021371e-16, 1.8277907e-14, 2.1894228e-15, 2.1912887e-09, 2.4131657e-15],
+    [7.9680637e-11, 4.9604531e-16, 1.1222097e-14, 7.0357717e-16, 8.1406737e-10, 1.8995133e-15],
+    [5.3185567e-15, 1.1295596e-16, 3.7382036e-16, 9.6017569e-11, 7.7980032e-11, 9.5400077e-17],
+    [7.7963183e-15, 1.1918470e-16, 4.6175255e-16, 1.2543247e-10, 4.1831982e-11, 9.2644248e-17],
+    [8.2702984e-15, 3.4756981e-16, 7.8200078e-16, 1.3178444e-10, 9.5712521e-11, 2.0128077e-16]
+]
+
+est_list = [0,0,0,0,5,5]
+gt_list = [5,0,0,3,4,5]
+
+# Create the folder if it doesn't exist
+
+folder_path = "./demo/matrix/"
+
+# Generate images with horizontal bars and save them in the folder
+for i, row in enumerate(matrix):
+    image = draw_horizontal_bars([row], est_list, gt_list)
+    image.save(os.path.join(folder_path, f"image_{i}.png"))
+'''
---
+++ demo/demo_timeline.py
@@ -0,0 +1,77 @@
+from PIL import Image, ImageDraw, ImageFont
+import matplotlib.font_manager as fm
+import os
+
+def create_timeline_image(list_names, skills, current_frame):
+    # Define the dimensions and colors for the image
+    width = 1820  # Image width in pixels
+    height = 250  # Image height in pixels
+    bg_color = (255, 255, 255)  # Background color (white)
+    legend_width = 80  # Width of the legend
+    list_name_width = 60  # Width of the list name section
+
+    # Define the skill names and colors
+    skill_names = ["bl", "fl", "flag", "ic", "mal", "none", "oafl", "oahs", "pl"]
+    skill_colors = [(220, 20, 60), (21, 176, 26), (3, 67, 223), (255, 170, 0), (19, 185, 191), (60, 60, 60), (169, 86, 30), (249, 115, 6), (218, 112, 214)]
+
+    # Calculate the width of each frame based on the number of frames and the image width
+    frame_width = (width - list_name_width - (legend_width)) // len(skills[0])
+
+    # Create a new image with extended width
+    image = Image.new("RGB", (width, height), bg_color)
+    draw = ImageDraw.Draw(image)
+
+    # Set the font for drawing text
+    environment = os.path.dirname(os.path.abspath(__file__))  # Get the current file directory
+    font_path = os.path.join(environment, "arial.ttf")
+    font_size1 = 20
+    font_size2 = 15
+    font1 = ImageFont.truetype(font_path, size=font_size1)
+    font2 = ImageFont.truetype(font_path, size=font_size2)
+
+    # Draw the list names on the left side
+    list_name_height = height // len(list_names)
+    for i, name in enumerate(list_names):
+        list_name_y = i * list_name_height
+        draw.text((5, list_name_y+30), name, fill=(0, 0, 0), font=font1, angle=90)
+
+    # Loop over each frame and assign the corresponding color
+    for i, skills_per_frame in enumerate(zip(*skills)):
+        # Calculate the position of the frame
+        x = list_name_width + i * frame_width
+        y = 0
+
+        # Define the color and name based on the skill values
+        for j, skill in enumerate(skills_per_frame):
+            if skill >= 0 and skill < len(skill_names):
+                color = skill_colors[skill]
+                name = skill_names[skill]
+            else:
+                color = (0, 0, 0)  # Default color for invalid skill values
+                name = ""
+
+            frame_y = y + (j * (height // len(list_names)))
+            frame_height = height // len(list_names)
+            image.paste(color, (x, frame_y, x + frame_width, frame_y + frame_height))
+
+        # Draw the cursor on the current frame
+        if i == current_frame:
+            cursor_x = x
+            cursor_y = y
+            cursor_height = height
+            draw.rectangle([(cursor_x, cursor_y), (cursor_x + frame_width, cursor_y + cursor_height)], fill=(255, 255, 255))
+
+    # Draw the legend on the right side
+    legend_x = x + (frame_width + 2)
+    legend_y = 0
+    legend_height = height
+    draw.rectangle([(legend_x, legend_y+2), (width, legend_y + legend_height+2)], fill=bg_color)
+
+    # Draw the legend labels
+    label_height = legend_height // len(skill_colors)
+    for i, color in enumerate(skill_colors):
+        label_y = legend_y + (i * label_height)
+        draw.rectangle([(legend_x, label_y+2), (width, label_y + label_height+2)], fill=color)
+        draw.text((legend_x + label_height, label_y+2), skill_names[i], fill=(255, 255, 255), font=font2)
+
+    return image
